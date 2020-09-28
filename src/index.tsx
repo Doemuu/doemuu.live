8d2b6cd1c54f8c769cc4c3da6f001f6f35231d38
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-09-28 21:10:57 +0200

    added basic firebase

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+
+import * as firebase from 'firebase';
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA4evUBL9vQufI73UA4A8HykURtoY-vO4I",
+  authDomain: "doemuulive-56e78.firebaseapp.com",
+  databaseURL: "https://doemuulive-56e78.firebaseio.com",
+  projectId: "doemuulive-56e78",
+  storageBucket: "doemuulive-56e78.appspot.com",
+  messagingSenderId: "534323793959",
+  appId: "1:534323793959:web:3005474a56f603c20c7e02",
+  measurementId: "G-4P3WK5GKEE",
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <Provider store={store}>
