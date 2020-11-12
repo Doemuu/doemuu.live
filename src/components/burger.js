30afee61dcfee01f47f40b1e78765bf5456dc791
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-11-12 01:19:35 +0100

    Implement Landingpage

---
+++ src/components/burger.js
@@ -0,0 +1,18 @@
+
+import React from "react"
+import "../style/burger.scss";
+
+const Burger = () => {
+
+  return (
+    <div className="navbtn">
+      <div className="btn-container">
+        <span className="line-1 line"></span>
+        <span className="line-2 line"></span>
+        <span className="line-3 line"></span>
+      </div>
+    </div>
+  )
+}
+
+export default Burger
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Burger from "../components/burger"
+import "../style/landingpage.scss"
 
 const IndexPage = () => (
-  <Layout>
+  <div className="body">
     <SEO title="Home" />
-    <h1>Hello World</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <Burger />
+    <div id="landingpage">
+      <div class="motto-container">
+        <div class="mottos">
+          <div class="motto-1">
+            <div class="spacer"></div>
+            <div class="motto">Loyalty</div>
+          </div>
+          <div class="motto-2">
+            <div class="spacer"></div>
+            <div class="motto">Courage</div>
+          </div>
+          <div class="motto-3">
+            <div class="spacer"></div>
+            <div class="motto">Endurance</div>
+          </div>
+        </div>
+        <div class="toolbar">
+          <Link class="btn">Learn More</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
