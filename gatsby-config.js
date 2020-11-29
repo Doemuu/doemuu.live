e88ebe7bb53199cfad46dc24a4584e2f2fc76674
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-11-29 20:14:20 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Loyalty. Courage. Endurance. Dominik Berger! British / Swiss Software Engineer based in Lucerne, Switzerland`,
     author: `@gatsbyjs`,
   },
   plugins: [
