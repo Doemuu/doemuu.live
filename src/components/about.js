381ad213b62b1dd4a149b5105fc480f425ef6db2
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-12-01 22:15:14 +0100

    Add aboutme image, layout

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,37 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import Img from "gatsby-image"
 
-const About = () => (
-  <div className="aboutme">
-    <div className="image">
+import "../style/about.scss"
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      profilepicture: file(relativePath: { eq: "me.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 940, maxWidth: 660) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="aboutme">
+      <div className="aboutme-content container-md d-flex">
+        <div className="image-container px-4">
+          <Img
+            className="image"
+            fluid={data?.profilepicture?.childImageSharp?.fluid}
+          />
+        </div>
+        <div className="chitchat">
+          <p>Hello World</p>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default About
--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,12 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
 import Timeline from "../components/timeline"
+import About from "../components/about"
 
 import "../style/cv.scss";
 
 const CV = () => (
   <Layout>
     <div className="cv">
+     <div className="aboutme">
+      <h2 className="title">About Me</h2>
+      <About/>
+     </div>
       <div className="timeline">
         <h2 className="title">Course of life</h2>
         <Timeline />
