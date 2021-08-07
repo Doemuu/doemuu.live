608d42f55da110de23bdc23de7ecfe2515766917
Doemuu <doemu@outlook.com>
2021-08-07 22:33:49 +0200

    add cleaner styles to tds map

--- src/pages/tour-de-suisse/index.tsx
+++ src/pages/tour-de-suisse/index.tsx
@@ -2,7 +2,11 @@ import React from "react"
 import Map from "../../components/cantons/map"
 
 const TDS = () => {
-  return <Map />
+  return (
+    <div className="map">
+      <Map />
+    </div>
+  )
 }
 
 export default TDS
